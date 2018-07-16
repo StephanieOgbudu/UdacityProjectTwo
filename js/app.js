ca7ec7164f07dabb3711965a47a48f5768f576e4
Stephanie Ogbudu <39443901+StephanieOgbudu@users.noreply.github.com>
2018-07-16 21:56:05 +0100

    Made adjustments

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $moves = $('.moves'),
 $timer = $('.timer'),
 $restart = $('.restart'),
 $deck = $('.deck'),
+currentTime,
 open = [],
 match = 0,
 second = 0,
@@ -18,29 +19,6 @@ totalCard = symbols.length / 2,
 stars3 = 14,
 stars2 = 16,
 star1 = 20;
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-function start() 
-{
-    let emojicard = shuffle(symbols);
-    $deck.empty();
-    moves = 0;
-    match = 0;
-    $moves.text('0');
-    for (let i = 0; i < emojicard.length; i++) 
-    {
-        $deck.append($('<li class="card"><i class="fa fa-' + emojicard[i] + '"></i></li>'))
-    }
-    addCardListener();
-}
-
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -57,7 +35,12 @@ function shuffle(array) {
     return array;
 }
 
-
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -68,6 +51,56 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+//start() function
+function start() 
+{
+    let emojicard = shuffle(symbols);
+    $deck.empty();
+    moves = 0;
+    match = 0;
+    $moves.text('0');
+    for (let i = 0; i < emojicard.length; i++) 
+    {
+        $deck.append($('<li class="card"><i class="fa fa-' + emojicard[i] + '"></i></li>'))
+    }
+    addCardListener();
+    resetTimer(currentTime);
+    second = 0;
+    $timer.text(`${second}`)
+    $("body").click(startTime());
+}
+
+//Rating Function
+function rating(moves) {
+    let rating = 3;
+    if (moves > stars3 && moves < stars2) {
+        $rating.eq(3).removeClass('fa-star').addClass('fa-star-o');
+    } else if (moves > stars2 && moves < star1) {
+        $rating.eq(2).removeClass('fa-star').addClass('fa-star-o');
+    } else if (moves > star1) {
+        $rating.eq(1).removeClass('fa-star').addClass('fa-star-o');
+        rating = 1;
+    }
+    return { score: rating };
+}
+
+function end(moves, score) 
+{
+    $('#winTime').text(`Time finished: ${second} seconds.`);
+    $('#winMove').text(`Moves: ${moves} moves`);
+    $('#winScore').text(`Score: ${score}`);
+    $('#winModal').modal('toggle');
+}
+
+$restart.bind('click', function (confirmed) 
+{
+    if (confirmed) 
+    {
+        $rating.removeClass('fa-star-o').addClass('fa-star');
+        start();
+    }
+});
+
 let addCardListener = function () 
 {
     $deck.find('.card').bind('click', function () 
@@ -116,40 +149,23 @@ let addCardListener = function ()
     });
 }
 
-
-//Rating Function
-function rating(moves) {
-    let rating = 3;
-    if (moves > stars3 && moves < stars2) {
-        $rating.eq(3).removeClass('fa-star').addClass('fa-star-o');
-    } else if (moves > stars2 && moves < star1) {
-        $rating.eq(2).removeClass('fa-star').addClass('fa-star-o');
-    } else if (moves > star1) {
-        $rating.eq(1).removeClass('fa-star').addClass('fa-star-o');
-        rating = 1;
-    }
-    return { score: rating };
-}
-
-function end(moves, score) 
-{
-    $('#winMove').text(`Moves: ${moves} moves`);
-    $('#winScore').text(`Score: ${score}`);
-    $('#winModal').modal('toggle');
-}
-
-$restart.bind('click', function (confirmed) 
-{
-    if (confirmed) 
-    {
-        $rating.removeClass('fa-star-o').addClass('fa-star');
-        start();
-    }
-});
-
-
-
 // Initiates the timer as soon as the game is loaded
+function startTime() 
+{
+    currentTime = setInterval(function () 
+    {
+        $timer.text(`${second}`)
+        second = second + 1
+    }, 1000);
+}
 
+// Resets the timer when the game ends or is restarted
+function resetTimer(timer) 
+{
+    if (timer) 
+    {
+        clearInterval(timer);
+    }
+}
 
 start();
